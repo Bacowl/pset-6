31a2df133ebfb64abf5d93442b08bb8a4b7d6574
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-12-12 13:20:15 -0500

    askdnsksj

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 window.onload = function() {
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("add").onclick = addItem;
+    document.getElementById("delete").onclick = deleteItem;
+    document.getElementById("priority").onclick = setPriority;
+    document.getElementById("complete").onclick = markComplete;
+}
+
+const list = []
+
+const addItem = function() {
+  var canvas = document.getElementById("list");
+  var ctx = canvas.getContext('2d');
+  var task = prompt("Task:")
+  list[list.length] = task;
+  ctx.font = "20px Georgia";
+  ctx.fillText(task[0], 20, 30)
 }
