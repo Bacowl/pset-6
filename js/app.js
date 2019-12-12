30e61b542ea9122a5846c1b566c25a0fccf49712
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-12-12 12:39:00 -0500

    dosjhfsdkjh

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+}
