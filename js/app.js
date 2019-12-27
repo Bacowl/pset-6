626aa9521eb00ba17cc21efc0dc47b45ac9d916d
Ben Carpenter <bacowl4017@gmail.com>
2019-12-26 23:56:30 -0500

    woooo

--- js/app.js
+++ js/app.js
@@ -9,12 +9,26 @@ const list = [];
 priorityList = [];
 
 const addItem = function() {
-  var task = "- " + prompt("Task:")
-  for (var i = 0; i < list.length; i++) {
-    var para= document.createElement("p");
+  if (list.length > 0) {
+    for (var i = 0; i < list.length; i++) {
+      var id2 = "p" + i
+      var element = document.getElementById(id2)
+      var parent = document.getElementById("div2")
+      console.log(element)
+      element.remove();
+    }
+  }
+  var task = prompt("Task:")
+  list[list.length] = task
+  var i = 0
+  while (i < list.length) {
+    var para = document.createElement("p");
+    var id = "p" + i
+    para.setAttribute("id", id)
     var node = document.createTextNode(list[i]);
     para.appendChild(node);
-    var element = document.getElementById("output-1");
+    var element = document.getElementById("div1");
     element.appendChild(para);
+    i++
   }
 }
