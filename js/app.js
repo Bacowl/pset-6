604a534449beba755e81337096c238361d582a91
Ben Carpenter <bacowl4017@gmail.com>
2019-12-27 16:14:26 -0500

    hegdsjfh

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ window.onload = function() {
     document.getElementById("priority").onclick = setPriority;
     document.getElementById("complete").onclick = markComplete;
 }
-
 const list = [];
 priorityList = [];
 
@@ -18,7 +17,9 @@ const addItem = function() {
     }
   }
   var task = prompt("Task:")
-  list[list.length] = task
+  if (task != null) {
+      list[list.length] = task
+  }
   var i = 0
   while (i < list.length) {
     var para = document.createElement("p");
@@ -26,10 +27,33 @@ const addItem = function() {
     para.setAttribute("id", id)
     para.style.color = "white";
     para.style.background = "grey";
+    para.style.border = "10px solid grey";
+    para.style.borderRadius = "20px";
+    para.style.font = "20px Times new Roman";
+    para.style.fontWeight = "bold";
     var node = document.createTextNode(list[i]);
     para.appendChild(node);
     var element = document.getElementById("div1");
     element.appendChild(para);
+
+    var del = document.createElement("button")
+    del.setAttribute("class", "delete")
+    var id3 = "Del" + i;
+    del.setAttribute("id", id3)
+    var node2 = document.createTextNode("\u274C")
+    var height = i*63 +251 + "px"
+    del.appendChild(node2);
+    del.style.position = "absolute";
+    del.style.top = height;
+    del.style.left = "200px";
+    del.style.background = "none"
+    del.style.border = "none"
+    element.insertAdjacentElement('afterBegin', del);
+    document.getElementById(id3).onclick = reply_click;
     i++
   }
+}
+
+const reply_click = function() {
+    alert(this.id);
 }
